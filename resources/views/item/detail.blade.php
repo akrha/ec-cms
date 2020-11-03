40b17b264f704523ecc9b05d85d4e80f6162be22
Akira <hase.a1995@gmail.com>
2020-11-03 19:20:49 +0900

    UI改善

--- resources/views/item/detail.blade.php
+++ resources/views/item/detail.blade.php
@@ -1,40 +1,38 @@
 @extends('common.main')
-商品詳細
 @section('title')
+商品詳細
 @endsection
-<a href="{{ route('items.createForm') }}">新規商品登録</a>
-    <a href="{{ route('items.index') }}">出品中商品一覧</a>
-    <h1>商品詳細ページ</h1>
-    <h2>{{ $item->item_name }}</h2>
-    説明：
-    <pre>{{ $item->description }}</pre>
-    価格：
-    <p>{{ $item->price }} 円</p>
-    登録日：
-    <p>{{ $item->created_at }}</p>
-    更新日：
-    <p>{{ $item->updated_at }}</p>
-    画像：
-    <p><img src="/{{ $item->photo_url }}" alt=""></p>
-    タグ：
-    @if ($tags)
-    @foreach ($tags as $tag)
-    <p>{{ $tag->name }}</p>
-    @endforeach
+@section('body')
+<div class="media">
+    @if ($item->photo_url)
+    <img src="/{{ $item->photo_url }}" class="img-thumbnail" style="width:30%">
+    @else
+    <img src="/no_photo.png"class="img-thumbnail w-3" style="width:30%">
     @endif
-    <p>{{ $item->tag_name }}</p>
-    <a href="{{ route('items.updateForm', ['item_id' => $item->id]) }}">商品編集</a>
-    {!! Form::open(['route' => ['items.destroy', 'item_id' => $item->id], 'id' => 'delete'.$item->id]) !!}
-    {!! Form::button('削除', ['onClick' => 'deleteConfirm('.$item->id.')']) !!}
-    {!! Form::close() !!}
+    <div class="media-body">
+        <h5 class="mt-0">{{ $item->item_name }}</h5>
+        {{ $item->description }}
+        <p style="color:red">{{ $item->price }} 円</p>
+        @if ($tags)
+        @foreach ($tags as $tag)
+        <p class="btn btn-warning">{{ $tag->name }}</p>
+        @endforeach
+        <br>
+        @endif
+        <a class="btn btn-success" href="{{ route('items.updateForm', ['item_id' => $item->id]) }}">商品編集</a>
+        {!! Form::open(['route' => ['items.destroy', 'item_id' => $item->id], 'id' => 'delete'.$item->id]) !!}
+        <button class="btn btn-danger" onclick="deleteConfirm({{ $item->id }})" type="button">削除</button>
+        {!! Form::close() !!}
+    </div>
+</div>
+
 
 <script>
 function deleteConfirm(item_id) {
     var res = confirm('本当に削除しますか？');
     if (res === true) {
         document.getElementById('delete'+item_id).submit();
-    } 
+    }
 }
 </script>
-@section('body')
 @endsection
